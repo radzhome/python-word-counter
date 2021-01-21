56a9c9da784890cb8959105e2eb3db615f7f1bf2
radzhome <radzhome@gmail.com>
2021-01-21 14:34:05 -0500

    add additional questions

--- README.md
+++ README.md
@@ -53,7 +53,7 @@ No setup is required. Either use the provided 'word_counter.py', or install and 
 
 ### Discussion ###
 
-* What is a word? An alphanumeric, numeric or alpha set of characters (minium 1), seperated by spaces, and punctuation is removed. Hyphenated words count as 1 i.e. `one-two` is 1 word, but `one two` is two. Punctuation such as `,\/*"'?!:` needs to be stripped away. Another scenario could be that we replace certain chars to make 1+ words out of a single word.
+* What is a word? An alphanumeric, numeric or alpha set of characters (minimum 1 char), seperated by spaces, and punctuation is removed. Hyphenated words count as 1 i.e. `one-two` is 1 word, but `one two` is two. Punctuation such as `,\/*"'?!:` needs to be stripped away. Another scenario could be that we replace certain chars to make 1+ words out of a single word.
 * In case of a tie between words, the word visited last (closest of EOF) will be bumped up in the list.
 * Instead of sorting the frequencies at the end, a top 10 list keeps track of the most frequently occurring words
 * For each word, up to 10 comparisons can be made, and the list needs to be shifted accordingly when an insert occurs
--- checker.py
+++ checker.py
@@ -1,3 +1,27 @@
+"""
+Auto checking for code challenge
+
+https://devskiller.com/interview-coding-challenges/#3_How_hard_should_your_hiring_coding_interview_challenges_be
+
+Additional questions:
+- Read the instructions!
+  - test suite included
+  - did not use 3rd party modules
+  - did not use any built in sorting or counting
+- Understood the requirements,
+- What technical decisions were made?
+- Asked any questions? Any assumptions identified?
+- What code coverage was achieved? (All functions tested?)
+- What business rules/cases were covered? (All business cases?)
+- How as storing top 10 achieved?
+- What data structures were used to leverage sorting?
+- Uniqueness of solution/problem solving i.e. regex, batch processing etc.
+- Organization of code? OO, functional, easy to read/follow
+- Defined the problem:
+  - case sensitivity
+  - dealing with punctuation
+  - hyphenated words, numbers
+"""
 import subprocess
 
 # TODO: Tailor the specific cases to exact test i.e. case sensitivity etc..
@@ -10,7 +34,6 @@ print(output)  # look at "maximum resident set size" for memory and "real" for r
 output = subprocess.check_output(['python3', 'word_counter.py', 'samples/test1.txt'])
 expected = b'Top 10 Words:\nsed 11\nipsum 9\nquis 9\neu 8\nip-sum 7\nvel 7\net 6\nultricies 5\nnunc 5\ntortor 5\nTotal Words: 326\n'
 assert output == expected, f"Output: {output}, Expected {expected}"
-
 
 # 2nd sample
 output = subprocess.check_output(['python3', 'word_counter.py', 'samples/test2.txt'])
